95881d2afdab7c5eca1b3a251e858569c035c900
raulBrady <bradyraule@gmail.com>
2020-11-15 11:18:39 -0500

    iniciando login registro

---
+++ JavaDesk/src/clases/JavaDesk.java
@@ -0,0 +1,18 @@
+package clases;
+
+import formularios.frmLogin;
+
+
+public class JavaDesk {
+
+    
+    public static void main(String[] args) 
+    {
+        frmLogin myLogin = new frmLogin();
+        myLogin.setLocationRelativeTo(null);
+        
+        myLogin.setVisible(true);
+    
+    }
+    
+}
